dd4ead2f76b22ea10116fcb0a109b232aaec7816
zahra taheri <zahratec50@gmail.com>
2025-04-03 22:14:38 -0700

    add route for Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import routes from './routes'
+
+import {useRouters} from 'react-router-dom'
 
 import './App.css'
 
 function App() {
-
+  const routers = useRouters(routes)
 
   return (
     <>
-      
+      {routers}
     </>
   )
 }
